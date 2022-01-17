4c8dc5a5deeb0014fa78539647edb0903fd0c3af
Evgen Bielich <belichevgenij@gmail.com>
2022-01-18 00:05:01 +0200

    del src

--- src/js/constants.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = 'https://pixabay.com/api';
-export const API_KEY = '24599473-e947cec96e5455781ec82382f';
--- src/js/refs.js
+++
@@ -1,7 +0,0 @@
-export const refs = {
-  searchForm: document.querySelector('#search-form'),
-  searchInput: document.querySelector('#search-form input'),
-  searchBtn: document.querySelector('#search-form button'),
-  gallery: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('.load-more'),
-};
